6c5905b820e68635c31b9c479d0c12bcf50ce1b4
Priyank raghwani <priyankraghwani98@gmail.com>
2020-06-06 16:21:49 +0530

    PipesExample

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { PipesComponentComponent } from './pipes-component/pipes-component.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'Pipes', component:PipesComponentComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = { PipesComponentComponent };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { flyingHeroes } from './class/flyingHeroes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PipesComponentComponent } from './pipes-component/pipes-component.component';
+
+import { Pipes } from './class/pipes';
+import { FlyingHeroesComponent } from './flying-heroes/flying-heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesComponentComponent,
+    FlyingHeroesComponent,
+    Pipes,
+    flyingHeroes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
